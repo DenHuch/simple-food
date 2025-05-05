685783cec74ff559bff48ad55e855755e98274c1
DenHuch <huchenko.denys@gmail.com>
2025-05-05 22:52:06 +0300

    added section hero and section categories

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const menuList = document.querySelector('.menu__list')
+	const categoriesFilter = document.querySelector('.categories__filter')
 
 	menuList.querySelectorAll('.menu-link').forEach(link => {
 		link.addEventListener('click', () => {
 			menuList.querySelectorAll('.menu-link').forEach(link => {
+				link.classList.remove('active')
+			})
+			link.classList.toggle('active')
+		})
+	})
+
+	categoriesFilter.querySelectorAll('.filter-btn').forEach(link => {
+		link.addEventListener('click', () => {
+			categoriesFilter.querySelectorAll('.filter-btn').forEach(link => {
 				link.classList.remove('active')
 			})
 			link.classList.toggle('active')
